f39a14561bf673e32d31c6bb9689c483e3fa5635
JoseMaksud <jf9718830@gmail.com>
2024-04-10 12:21:37 -0300

    Teste

---
+++ pokedex/App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
+import Card from './src/components/Card'
+import { pokemons } from './src/data/pokemons';
+
+export default function App() {
+  const renderPokemon = ({ item }) => (
+    <Card pokemon={item} key={item.Numero} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Header />
+        <FlatList
+          data={pokemons}
+          style={styles.cards}
+          keyExtractor={(pokemon) => pokemon.Numero.toString() }
+          renderItem={renderPokemon}>
+          </FlatList>
+          <StatusBar style='auto' />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'column'
+  },
+  cards: {
+    padding: 15,
+    marginBottom: 30,
+  }
+});
