8346148e3053e7b0fee9b2d390b7b5382f1e9419
JoseMaksud <jf9718830@gmail.com>
2024-04-10 12:23:35 -0300

    Funcionando

--- pokedex/App.js
+++ pokedex/App.js
@@ -3,7 +3,7 @@ import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
 import Card from './src/components/Card'
 import { pokemons } from './src/data/pokemons';
-
+// Projeto
 export default function App() {
   const renderPokemon = ({ item }) => (
     <Card pokemon={item} key={item.Numero} />
